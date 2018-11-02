4ecb1b53b2a0383698dc0ec3d680e2f0ddf3ccbd
David <gr3atdane@gmail.com>
2018-11-02 21:13:12 +0900

    Add files via upload

--- Japanese Numbers.py
+++ Japanese Numbers.py
@@ -1,6 +1,7 @@
 # Japanese Number Converter
 # currently using length functions - possible to use Recursive Functions?
-# Works only up to sen man for now.
+# Works only up to sen man for now (9 digits)
+# Now includes Kanji to arabic numbers, WIP
 
 romaji_dict = {"0": "zero", "1": "ichi", "2": "ni", "3": "san", "4": "yon", "5": "go", "6": "roku", "7": "nana",
                "8": "hachi", "9": "kyuu", "10": "juu", "100": "hyaku", "1000": "sen", "10000": "man", "100000000": "oku",
@@ -166,28 +167,112 @@ def remove_spaces(convert_result):
     return correction
 
 
+def kanjiConvert(convert_num):
+    # Converts kanji to arabic number
+    
+    if convert_num = "零":
+        return 0
+
+    # First, needs to check for MAN 万 and OKU 億 kanji, as need to handle differently, splitting up the numbers at these intervals.
+    # key tells us whether we need to add or multiple the numbers, then we create a list of numbers in an order we need to multipy/add
+    key = []
+    numberList = []
+    y = ""
+    for x in convert_num:
+        if x == "万" or x == "億":
+            numberList.append(y)
+            key.append("times")
+            numberList.append(x)
+            key.append("plus")
+            y = ""
+        else:
+            y += x
+    if y != "":
+        numberList.append(y)
+
+    numberListConverted = []
+    baseNumber = ["一", "二", "三", "四", "五", "六", "七", "八", "九"]
+    linkNumber = ["十", "百", "千", "万", "億"]
+
+    # Converts the kanji number list to arabic numbers, using the 'base number' and 'link number' list above. For a link number, we would need to
+    # link with a base number
+    for noX in numberList:
+        count = len(noX)
+        result = 0
+        skip = 1
+        for x in reversed(noX):
+            addTo = 0
+            skip -= 1
+            count = count - 1
+            if skip == 1:
+                continue
+            if x in baseNumber:
+                for y, z in kanji_dict.items():
+                    if z == x:
+                        result += int(y)
+            elif x in linkNumber:
+                if noX[count - 1] in baseNumber and count > 0:
+                    for y, z in kanji_dict.items():
+                        if z == noX[count - 1]:
+                            tempNo = int(y)
+                            for y, z in kanji_dict.items():
+                                if z == x:
+                                    addTo += tempNo * int(y)
+                                    result += addTo
+                                    skip = 2
+                else:
+                    for y, z in kanji_dict.items():
+                        if z == x:
+                            result += int(y)
+        numberListConverted.append(int(result))
+
+    result = numberListConverted[0]
+    y = 0
+
+    # iterate over the converted list, and either multiply/add as instructed in key list
+    for x in range(1,len(numberListConverted)):
+        if key[y] == "plus":
+            try:
+                if key[y+1] == "times":
+                    result = result + numberListConverted[x] * numberListConverted[x+1]
+                    y += 1
+                else:
+                    result += numberListConverted[x]
+            except IndexError:
+                result += numberListConverted[-1]
+                break
+        else:
+            result = result * numberListConverted[x]
+        y += 1
+
+    return result
+
+
 # Start
-convert_num = input("Enter a Western number: ")
-# Exit if length is greater than current limit
-if len(convert_num) > 9:
-    print("Number length too long, choose less than 10 digits")
-    exit()
+convert_num = input("Enter a Western (arabic) number or Japanese Kanji: ")
+print("Your requested Number: " + convert_num)
 
 # Get rid of any zeros at the front
-print("Your requested Number: " + convert_num)
 while convert_num[0] == "0":
     convert_num = convert_num[1:]
 
-# Kanji
-requested_dict = kanji_dict
-convert_result = convert(convert_num)
-convert_result = remove_spaces(convert_result)
-print("Kanji: " + convert_result)
-# Hiragana
-requested_dict = hiragana_dict
-convert_result = convert(convert_num)
-convert_result = remove_spaces(convert_result)
-print("Hiragana: " + convert_result)
-# Romaji
-requested_dict = romaji_dict
-print("Romaji: " + convert(convert_num))
+# Exit if length is greater than current limit
+if convert_num[0] in kanji_dict.values():
+    print("Western number: " + str(kanjiConvert(convert_num)))
+elif len(convert_num) > 9:
+    print("Number length too long, choose less than 10 digits")
+    exit()
+else:
+    # Kanji
+    requested_dict = kanji_dict
+    convert_result = convert(convert_num)
+    convert_result = remove_spaces(convert_result)
+    print("Kanji: " + convert_result)
+    # Hiragana
+    requested_dict = hiragana_dict
+    convert_result = convert(convert_num)
+    convert_result = remove_spaces(convert_result)
+    print("Hiragana: " + convert_result)
+    # Romaji
+    requested_dict = romaji_dict
+    print("Romaji: " + convert(convert_num))
